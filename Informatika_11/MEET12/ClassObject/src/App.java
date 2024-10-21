cc245ea2dd62d88ebc7ab39f341404b9f3d85b1f
nelshenyong <nelshenyong@gmail.com>
2024-10-21 10:43:15 +0700

    update

--- Informatika_11/MEET12/ClassObject/src/App.java
+++ Informatika_11/MEET12/ClassObject/src/App.java
@@ -10,9 +10,19 @@ public class App {
         {
             Siswa siswa01 = new Siswa();
             siswa01.nama = "Martin";
-            siswa01.kelas = "IPA 1";
+            siswa01.kelas = "IPA 6";
             siswa01.nim = 12345;
-            siswa01.nilai = 99.5;
+            siswa01.nilai = 100;
+            System.out.println("Hai" + siswa01.nama + " saya kelas "  + siswa01.kelas + " dengan NIM " + siswa01.nim + " dengan nilai " + siswa01.nilai);   
+            
+            System.out.println("------------------");
+            
+            Siswa siswa02 = new Siswa();
+            siswa02.nama = "Felix";
+            siswa02.kelas = "IPA 1";
+            siswa02.nim = 911;
+            siswa02.nilai = 0;
+            System.out.println("Hai" + siswa02.nama + " saya kelas "  + siswa02.kelas + " dengan NIM " + siswa02.nim + " dengan nilai " + siswa02.nilai);
         }
     }
 }
